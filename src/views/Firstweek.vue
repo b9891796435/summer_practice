119376abfcc06cc54c43fc378148ad61169035ef
b9891796435 <1362282435@qq.com>
2021-07-18 23:52:29 +0800

    Added some thought

--- src/views/Firstweek.vue
+++ src/views/Firstweek.vue
@@ -10,7 +10,7 @@
     msg: String
   }
 }</code><br/>
-如此即可在子组件中以类似data字段的使用方法使用props。如下例：
+如此即可在子组件中以类似data字段的使用方法使用props。如下例中，输入内容后点击按钮便会创建一个组件并传入输入值作为参数：
     </p>
     <el-input v-model="input" placeholder="请输入内容">
       <template #append>
@@ -20,6 +20,10 @@
     <li v-for="i in magic" :key="i.message">
       <HelloWorld  :msg="i.message"/>
     </li>
+    <h2>遇到的问题</h2>
+      <p>主要问题来源于过长时间没有使用Vue组件导致语法混乱以及Vscode的背刺。组件语法错误使用F12看报错加百度以及查看文档解决，Vscode背刺使用百度/谷歌解决。</p>
+    <h2>其他感受</h2>
+    <p>太长时间不写东西果然会手生，<del>死线是第一生产力（bushi</del></p>
   </div>
 </template>
 
@@ -47,3 +51,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+div{
+  text-align: left;
+}
+</style>
